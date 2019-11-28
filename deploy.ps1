03c5bd8d582cd32bb446bfe102e121f0909de587
Meulensteen <884321@fontys.nl>
2019-11-28 15:45:06 +0100

    changed ingress and deployment powershell

--- deploy.ps1
+++ deploy.ps1
@@ -2,7 +2,7 @@
 $dockerrepo = "zephinzer/eg-whack-a-pod"
 
 
-kubectl run admin-deployment --image="$dockerrepo":admin --replicas=1 --port=8080 --labels=app=admin --env="APIIMAGE=$dockerrepo:api"
+kubectl run admin-deployment --image="$dockerrepo":admin --replicas=1 --port=8080 --labels=app=admin --env="APIIMAGE="$dockerrepo":api"
 kubectl expose deployment admin-deployment --name=admin --target-port=8080  --type=NodePort --labels="app=admin"
 #kubectl create serviceaccount wap-admin	
 #kubectl create clusterrolebinding wap-admin --clusterrole=cluster-admin --serviceaccount=$$(kubectl config view -o jsonpath="{.contexts[?(@.name==\"$$(kubectl config current-context)\")].context.namespace}"):wap-admin
